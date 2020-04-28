d9cf8759bd0a781e60887d8ee6fce8c4474000e4
4Korn <4Kornev@gmail.com>
2020-04-28 21:51:09 +0300

    Initial commit

---
+++ src/sample/Main.java
@@ -0,0 +1,24 @@
+package sample;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent login = FXMLLoader.load(getClass().getResource("login.fxml"));
+        primaryStage.setTitle("УД Бюджет");
+        primaryStage.setScene(new Scene(login, 250, 300));
+        primaryStage.show();
+        primaryStage.setResizable(false);//запрещаем менять размер окна
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
