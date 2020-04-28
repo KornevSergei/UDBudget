327f00691d62c674643dc0a0e6cb52c9928ea213
4Korn <4Kornev@gmail.com>
2020-04-29 01:20:31 +0300

    Commit 3

--- src/sample/LoginController.java
+++ src/sample/LoginController.java
@@ -1,11 +1,16 @@
 package sample;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.stage.Stage;
 
 public class LoginController {
 
@@ -28,5 +33,22 @@ public class LoginController {
 
     public void login(ActionEvent actionEvent) {
         System.out.println("sdpmsnvoindob");
+        loginSignUpButton.getScene().getWindow().hide();//прячем окно
+
+
+        //создаём новое окно
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(getClass().getResource("/sample/CreateProject.fxml"));
+
+        try {
+            loader.load();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+            Parent root = loader.getRoot();
+            Stage stage = new Stage();
+            stage.setScene(new Scene(root));
+            stage.showAndWait();//отображение с задержкой прошлого окна
+
     }
 }
