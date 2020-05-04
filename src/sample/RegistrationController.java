a0ae4773196c9a1b6074dae3b3717dc48b0d3535
4Korn <4Kornev@gmail.com>
2020-05-04 09:21:00 +0300

    Commit 15

--- src/sample/RegistrationController.java
+++ src/sample/RegistrationController.java
@@ -25,7 +25,7 @@ public class RegistrationController {
     TextField signUpMail;
     TextField signUpLogin;
     TextField signUpPassword;
-    
+
     Button registrationButton;
 
 
@@ -36,15 +36,51 @@ public class RegistrationController {
 
     public void registration(ActionEvent actionEvent) throws Exception {
 
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        Connection con = DriverManager.getConnection("jdbc:mysql://localhost/user?useUnicode=true&serverTimezone=UTC&useSSL=false", "root", "root");
+        String query = "INSERT INTO users (phone, mail, login, password)" + "VALUES (? ,?,?,?)";
+
         try {
-            Class.forName("com.mysql.cj.jdbc.Driver");
-            Connection con = DriverManager.getConnection("jdbc:mysql://localhost/user?useUnicode=true&serverTimezone=UTC&useSSL=false", "root", "root");
-            Statement statement = con.createStatement();
-            statement.execute("INSERT INTO users (phone, mail, login, password) VALUES (" + signUpPhone.getText() + "\', \'"
-                    + signUpMail.getText() + "\', \'" + signUpLogin.getText() + "\', \'" + signUpPassword.getText() + "\', \'");
+
+            PreparedStatement pst = con.prepareStatement(query);
+            pst.setString(1, signUpPhone.getText());
+            pst.setString(2, signUpMail.getText());
+            pst.setString(3, signUpLogin.getText());
+            pst.setString(4, signUpPassword.getText());
+
+            pst.executeUpdate();
+
         } catch (SQLException el) {
             el.printStackTrace();
         }
+
+
+//        try {
+//            String query = "INSERT INTO users (phone, mail, login, password) VALUES (?,?,?,?)";
+//            Class.forName("com.mysql.cj.jdbc.Driver");
+//            Connection con = DriverManager.getConnection("jdbc:mysql://localhost/user?useUnicode=true&serverTimezone=UTC&useSSL=false", "root", "root");
+//
+//            PreparedStatement pst = con.prepareStatement(query);
+//            pst.setString(1, signUpPhone.getText());
+//            pst.setString(2, signUpMail.getText());
+//            pst.setString(3, signUpLogin.getText());
+//            pst.setString(4, signUpPassword.getText());
+//
+//            pst.executeUpdate();
+//
+//        } catch (SQLException el) {
+//            el.printStackTrace();
+//        }
+
+
+//        try {
+//            Class.forName("com.mysql.cj.jdbc.Driver");
+//            Connection con = DriverManager.getConnection("jdbc:mysql://localhost/user?useUnicode=true&serverTimezone=UTC&useSSL=false", "root", "root");
+//            Statement statement = con.createStatement();
+//            statement.execute("INSERT INTO users (phone, mail, login, password) VALUES (1212,2321,23,43)");
+//        } catch (SQLException el) {
+//            el.printStackTrace();
+//        }
 
 
 //            registrationButton.getScene().getWindow().hide();//после клика закрываекм окно
