7a3675f7e8eb8659851904917303efdad454e98a
Korn <4Kornev@gmail.com>
2020-05-20 16:35:25 +0300

    Commit 57

--- src/sample/EditProjectController.java
+++ src/sample/EditProjectController.java
@@ -30,6 +30,24 @@ public class EditProjectController implements Initializable {
     public TableColumn<Furniture, Boolean> tabcolActiveC;
     public TableColumn<Furniture, String> colUnitFurniture;
     public TableColumn<Furniture, String> colQuantityFurniture;
+    public TableColumn<Furniture, String> colOrdinalPricePerUnitFurniture;
+    public TableColumn<Furniture, String> colPriceCPPerUnitFurniture;
+    public TableColumn<Furniture, String> colPriceCPPerKeyFurniture;
+    public TableColumn<Furniture, String> colCostCPPerUnitFurniture;
+    public TableColumn<Furniture, String> colPriceOrderFurniture;
+    public TableColumn<Furniture, String> colCostCPFurniture;
+    public TableColumn<Furniture, String> colProductionTimeFurniture;
+    public TableColumn<Furniture, String> colActualCostFurniture;
+    public TableColumn<Furniture, String> colActualDifferenceFurniture;
+    public TableColumn<Furniture, String> colPaidFurniture;
+    public TableColumn<Furniture, String> colResidueFurniture;
+    public TableColumn<Furniture, String> colDateOfDeliveryFurniture;
+    public TableColumn<Furniture, String> colPlannedCPFurniture;
+    public TableColumn<Furniture, String> colActualCPFurniture;
+    public TableColumn<Furniture, String> colAccountFurniture;
+    public TableColumn<Furniture, String> colContactsFurniture;
+    public TableColumn<Furniture, String> colNotesFurniture;
+    public TableColumn<Furniture, String> colCharacteristicsFurniture;
 
 
     public Button createRoomButton;
@@ -41,23 +59,55 @@ public class EditProjectController implements Initializable {
         colNameFurniture.setCellValueFactory(new PropertyValueFactory<>("nameFurniture"));
         colUnitFurniture.setCellValueFactory(new PropertyValueFactory<>("unitFurniture"));
         colQuantityFurniture.setCellValueFactory(new PropertyValueFactory<>("quantityFurniture"));
-
-
-
+        colOrdinalPricePerUnitFurniture.setCellValueFactory(new PropertyValueFactory<>("ordinalPricePerUnitFurniture"));
+        colPriceCPPerUnitFurniture.setCellValueFactory(new PropertyValueFactory<>("priceCPPerUnitFurniture"));
+        colPriceCPPerKeyFurniture.setCellValueFactory(new PropertyValueFactory<>("priceCPPerKeyFurniture"));
+        colCostCPPerUnitFurniture.setCellValueFactory(new PropertyValueFactory<>("costCPPerUnitFurniture"));
+        colPriceOrderFurniture.setCellValueFactory(new PropertyValueFactory<>("priceOrderFurniture"));
+        colCostCPFurniture.setCellValueFactory(new PropertyValueFactory<>("costCPFurniture"));
+        colProductionTimeFurniture.setCellValueFactory(new PropertyValueFactory<>("productionTimeFurniture"));
+        colActualCostFurniture.setCellValueFactory(new PropertyValueFactory<>("actualCostFurniture"));
+        colActualDifferenceFurniture.setCellValueFactory(new PropertyValueFactory<>("actualDifferenceFurniture"));
+        colPaidFurniture.setCellValueFactory(new PropertyValueFactory<>("paidFurniture"));
+        colResidueFurniture.setCellValueFactory(new PropertyValueFactory<>("residueFurniture"));
+        colDateOfDeliveryFurniture.setCellValueFactory(new PropertyValueFactory<>("dateOfDeliveryFurniture"));
+        colPlannedCPFurniture.setCellValueFactory(new PropertyValueFactory<>("plannedCPFurniture"));
+        colActualCPFurniture.setCellValueFactory(new PropertyValueFactory<>("actualCPFurniture"));
+        colAccountFurniture.setCellValueFactory(new PropertyValueFactory<>("accountFurniture"));
+        colContactsFurniture.setCellValueFactory(new PropertyValueFactory<>("contactsFurniture"));
+        colNotesFurniture.setCellValueFactory(new PropertyValueFactory<>("notesFurniture"));
+        colCharacteristicsFurniture.setCellValueFactory(new PropertyValueFactory<>("characteristicsFurniture"));
 
         furnitureTableView.setItems(observableList);
-
-
         furnitureTableView.setEditable(true);
+
         colNameFurniture.setCellFactory(TextFieldTableCell.forTableColumn());
         colUnitFurniture.setCellFactory(TextFieldTableCell.forTableColumn());
         colQuantityFurniture.setCellFactory(TextFieldTableCell.forTableColumn());
+        colOrdinalPricePerUnitFurniture.setCellFactory(TextFieldTableCell.forTableColumn());
+        colPriceCPPerUnitFurniture.setCellFactory(TextFieldTableCell.forTableColumn());
+        colPriceCPPerKeyFurniture.setCellFactory(TextFieldTableCell.forTableColumn());
+        colCostCPPerUnitFurniture.setCellFactory(TextFieldTableCell.forTableColumn());
+        colPriceOrderFurniture.setCellFactory(TextFieldTableCell.forTableColumn());
+        colCostCPFurniture.setCellFactory(TextFieldTableCell.forTableColumn());
+        colProductionTimeFurniture.setCellFactory(TextFieldTableCell.forTableColumn());
+        colActualCostFurniture.setCellFactory(TextFieldTableCell.forTableColumn());
+        colActualDifferenceFurniture.setCellFactory(TextFieldTableCell.forTableColumn());
+        colPaidFurniture.setCellFactory(TextFieldTableCell.forTableColumn());
+        colResidueFurniture.setCellFactory(TextFieldTableCell.forTableColumn());
+        colDateOfDeliveryFurniture.setCellFactory(TextFieldTableCell.forTableColumn());
+        colPlannedCPFurniture.setCellFactory(TextFieldTableCell.forTableColumn());
+        colActualCPFurniture.setCellFactory(TextFieldTableCell.forTableColumn());
+        colAccountFurniture.setCellFactory(TextFieldTableCell.forTableColumn());
+        colContactsFurniture.setCellFactory(TextFieldTableCell.forTableColumn());
+        colNotesFurniture.setCellFactory(TextFieldTableCell.forTableColumn());
+        colCharacteristicsFurniture.setCellFactory(TextFieldTableCell.forTableColumn());
     }
 
     ObservableList<Furniture> observableList = FXCollections.observableArrayList(
-            new Furniture("Ведро", "шт", "2"),
-            new Furniture("Лопата", "шт", "5"),
-            new Furniture("", "", "")
+//            new Furniture("Ведро", "шт", "2"),
+//            new Furniture("Лопата", "шт", "5"),
+            new Furniture()
 
     );
 
--- src/sample/Furniture.java
+++ src/sample/Furniture.java
@@ -6,14 +6,55 @@ public class Furniture {
     protected SimpleStringProperty nameFurniture;
     protected SimpleStringProperty unitFurniture;
     protected SimpleStringProperty quantityFurniture;
+    protected SimpleStringProperty ordinalPricePerUnitFurniture;
+    protected SimpleStringProperty priceCPPerUnitFurniture;
+    protected SimpleStringProperty priceCPPerKeyFurniture;
+    protected SimpleStringProperty costCPPerUnitFurniture;
+    protected SimpleStringProperty priceOrderFurniture;
+    protected SimpleStringProperty costCPFurniture;
+    protected SimpleStringProperty productionTimeFurniture;
+    protected SimpleStringProperty actualCostFurniture;
+    protected SimpleStringProperty actualDifferenceFurniture;
+    protected SimpleStringProperty paidFurniture;
+    protected SimpleStringProperty residueFurniture;
+    protected SimpleStringProperty dateOfDeliveryFurniture;
+    protected SimpleStringProperty plannedCPFurniture;
+    protected SimpleStringProperty actualCPFurniture;
+    protected SimpleStringProperty accountFurniture;
+    protected SimpleStringProperty contactsFurniture;
+    protected SimpleStringProperty notesFurniture;
+    protected SimpleStringProperty characteristicsFurniture;
+
 
     public Furniture() {
     }
 
-    public Furniture(String nameFurniture, String unitFurniture, String quantityFurniture) {
+
+    public Furniture(String nameFurniture, String unitFurniture, String quantityFurniture, String ordinalPricePerUnitFurniture, String priceCPPerUnitFurniture,
+                     String priceCPPerKeyFurniture, String costCPPerUnitFurniture, String priceOrderFurniture, String costCPFurniture, String productionTimeFurniture,
+                     String actualCostFurniture, String actualDifferenceFurniture, String paidFurniture, String residueFurniture, String dateOfDeliveryFurniture,
+                     String plannedCPFurniture, String actualCPFurniture, String accountFurniture, String contactsFurniture, String notesFurniture, String characteristicsFurniture) {
         this.nameFurniture = new SimpleStringProperty(nameFurniture);
         this.unitFurniture = new SimpleStringProperty(unitFurniture);
         this.quantityFurniture = new SimpleStringProperty(quantityFurniture);
+        this.ordinalPricePerUnitFurniture = new SimpleStringProperty(ordinalPricePerUnitFurniture);
+        this.priceCPPerUnitFurniture = new SimpleStringProperty(priceCPPerUnitFurniture);
+        this.priceCPPerKeyFurniture = new SimpleStringProperty(priceCPPerKeyFurniture);
+        this.costCPPerUnitFurniture = new SimpleStringProperty(costCPPerUnitFurniture);
+        this.priceOrderFurniture = new SimpleStringProperty(priceOrderFurniture);
+        this.costCPFurniture = new SimpleStringProperty(costCPFurniture);
+        this.productionTimeFurniture = new SimpleStringProperty(productionTimeFurniture);
+        this.actualCostFurniture = new SimpleStringProperty(actualCostFurniture);
+        this.actualDifferenceFurniture = new SimpleStringProperty(actualDifferenceFurniture);
+        this.paidFurniture = new SimpleStringProperty(paidFurniture);
+        this.residueFurniture = new SimpleStringProperty(residueFurniture);
+        this.dateOfDeliveryFurniture = new SimpleStringProperty(dateOfDeliveryFurniture);
+        this.plannedCPFurniture = new SimpleStringProperty(plannedCPFurniture);
+        this.actualCPFurniture = new SimpleStringProperty(actualCPFurniture);
+        this.accountFurniture = new SimpleStringProperty(accountFurniture);
+        this.contactsFurniture = new SimpleStringProperty(contactsFurniture);
+        this.notesFurniture = new SimpleStringProperty(notesFurniture);
+        this.characteristicsFurniture = new SimpleStringProperty(characteristicsFurniture);
     }
 
     public String getNameFurniture() {
@@ -37,7 +78,7 @@ public class Furniture {
     }
 
     public void setUnitFurniture(String unitFurniture) {
-        this.unitFurniture = new SimpleStringProperty(unitFurniture);
+        this.unitFurniture.set(unitFurniture);
     }
 
     public String getQuantityFurniture() {
@@ -49,6 +90,222 @@ public class Furniture {
     }
 
     public void setQuantityFurniture(String quantityFurniture) {
-        this.quantityFurniture = new SimpleStringProperty(quantityFurniture);
+        this.quantityFurniture.set(quantityFurniture);
+    }
+
+    public String getOrdinalPricePerUnitFurniture() {
+        return ordinalPricePerUnitFurniture.get();
+    }
+
+    public SimpleStringProperty ordinalPricePerUnitFurnitureProperty() {
+        return ordinalPricePerUnitFurniture;
+    }
+
+    public void setOrdinalPricePerUnitFurniture(String ordinalPricePerUnitFurniture) {
+        this.ordinalPricePerUnitFurniture.set(ordinalPricePerUnitFurniture);
+    }
+
+    public String getPriceCPPerUnitFurniture() {
+        return priceCPPerUnitFurniture.get();
+    }
+
+    public SimpleStringProperty priceCPPerUnitFurnitureProperty() {
+        return priceCPPerUnitFurniture;
+    }
+
+    public void setPriceCPPerUnitFurniture(String priceCPPerUnitFurniture) {
+        this.priceCPPerUnitFurniture.set(priceCPPerUnitFurniture);
+    }
+
+    public String getPriceCPPerKeyFurniture() {
+        return priceCPPerKeyFurniture.get();
+    }
+
+    public SimpleStringProperty priceCPPerKeyFurnitureProperty() {
+        return priceCPPerKeyFurniture;
+    }
+
+    public void setPriceCPPerKeyFurniture(String priceCPPerKeyFurniture) {
+        this.priceCPPerKeyFurniture.set(priceCPPerKeyFurniture);
+    }
+
+    public String getCostCPPerUnitFurniture() {
+        return costCPPerUnitFurniture.get();
+    }
+
+    public SimpleStringProperty costCPPerUnitFurnitureProperty() {
+        return costCPPerUnitFurniture;
+    }
+
+    public void setCostCPPerUnitFurniture(String costCPPerUnitFurniture) {
+        this.costCPPerUnitFurniture.set(costCPPerUnitFurniture);
+    }
+
+    public String getPriceOrderFurniture() {
+        return priceOrderFurniture.get();
+    }
+
+    public SimpleStringProperty priceOrderFurnitureProperty() {
+        return priceOrderFurniture;
+    }
+
+    public void setPriceOrderFurniture(String priceOrderFurniture) {
+        this.priceOrderFurniture.set(priceOrderFurniture);
+    }
+
+    public String getCostCPFurniture() {
+        return costCPFurniture.get();
+    }
+
+    public SimpleStringProperty costCPFurnitureProperty() {
+        return costCPFurniture;
+    }
+
+    public void setCostCPFurniture(String costCPFurniture) {
+        this.costCPFurniture.set(costCPFurniture);
+    }
+
+    public String getProductionTimeFurniture() {
+        return productionTimeFurniture.get();
+    }
+
+    public SimpleStringProperty productionTimeFurnitureProperty() {
+        return productionTimeFurniture;
+    }
+
+    public void setProductionTimeFurniture(String productionTimeFurniture) {
+        this.productionTimeFurniture.set(productionTimeFurniture);
+    }
+
+    public String getActualCostFurniture() {
+        return actualCostFurniture.get();
+    }
+
+    public SimpleStringProperty actualCostFurnitureProperty() {
+        return actualCostFurniture;
+    }
+
+    public void setActualCostFurniture(String actualCostFurniture) {
+        this.actualCostFurniture.set(actualCostFurniture);
+    }
+
+    public String getActualDifferenceFurniture() {
+        return actualDifferenceFurniture.get();
+    }
+
+    public SimpleStringProperty actualDifferenceFurnitureProperty() {
+        return actualDifferenceFurniture;
+    }
+
+    public void setActualDifferenceFurniture(String actualDifferenceFurniture) {
+        this.actualDifferenceFurniture.set(actualDifferenceFurniture);
+    }
+
+    public String getPaidFurniture() {
+        return paidFurniture.get();
+    }
+
+    public SimpleStringProperty paidFurnitureProperty() {
+        return paidFurniture;
+    }
+
+    public void setPaidFurniture(String paidFurniture) {
+        this.paidFurniture.set(paidFurniture);
+    }
+
+    public String getResidueFurniture() {
+        return residueFurniture.get();
+    }
+
+    public SimpleStringProperty residueFurnitureProperty() {
+        return residueFurniture;
+    }
+
+    public void setResidueFurniture(String residueFurniture) {
+        this.residueFurniture.set(residueFurniture);
+    }
+
+    public String getDateOfDeliveryFurniture() {
+        return dateOfDeliveryFurniture.get();
+    }
+
+    public SimpleStringProperty dateOfDeliveryFurnitureProperty() {
+        return dateOfDeliveryFurniture;
+    }
+
+    public void setDateOfDeliveryFurniture(String dateOfDeliveryFurniture) {
+        this.dateOfDeliveryFurniture.set(dateOfDeliveryFurniture);
+    }
+
+    public String getPlannedCPFurniture() {
+        return plannedCPFurniture.get();
+    }
+
+    public SimpleStringProperty plannedCPFurnitureProperty() {
+        return plannedCPFurniture;
+    }
+
+    public void setPlannedCPFurniture(String plannedCPFurniture) {
+        this.plannedCPFurniture.set(plannedCPFurniture);
+    }
+
+    public String getActualCPFurniture() {
+        return actualCPFurniture.get();
+    }
+
+    public SimpleStringProperty actualCPFurnitureProperty() {
+        return actualCPFurniture;
+    }
+
+    public void setActualCPFurniture(String actualCPFurniture) {
+        this.actualCPFurniture.set(actualCPFurniture);
+    }
+
+    public String getAccountFurniture() {
+        return accountFurniture.get();
+    }
+
+    public SimpleStringProperty accountFurnitureProperty() {
+        return accountFurniture;
+    }
+
+    public void setAccountFurniture(String accountFurniture) {
+        this.accountFurniture.set(accountFurniture);
+    }
+
+    public String getContactsFurniture() {
+        return contactsFurniture.get();
+    }
+
+    public SimpleStringProperty contactsFurnitureProperty() {
+        return contactsFurniture;
+    }
+
+    public void setContactsFurniture(String contactsFurniture) {
+        this.contactsFurniture.set(contactsFurniture);
+    }
+
+    public String getNotesFurniture() {
+        return notesFurniture.get();
+    }
+
+    public SimpleStringProperty notesFurnitureProperty() {
+        return notesFurniture;
+    }
+
+    public void setNotesFurniture(String notesFurniture) {
+        this.notesFurniture.set(notesFurniture);
+    }
+
+    public String getCharacteristicsFurniture() {
+        return characteristicsFurniture.get();
+    }
+
+    public SimpleStringProperty characteristicsFurnitureProperty() {
+        return characteristicsFurniture;
+    }
+
+    public void setCharacteristicsFurniture(String characteristicsFurniture) {
+        this.characteristicsFurniture.set(characteristicsFurniture);
     }
 }
